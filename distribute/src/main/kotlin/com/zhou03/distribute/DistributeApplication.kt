33b730d894189d49634e71fe92658432ea0f1c43
zhou03 <1093826740@qq.com>
2024-05-13 22:50:36 +0800

    Initial commit

---
+++ distribute/src/main/kotlin/com/zhou03/distribute/DistributeApplication.kt
@@ -0,0 +1,11 @@
+package com.zhou03.distribute
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DistributeApplication
+
+fun main(args: Array<String>) {
+	runApplication<DistributeApplication>(*args)
+}
---
+++ distribute/src/test/kotlin/com/zhou03/distribute/DistributeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.zhou03.distribute
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DistributeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
