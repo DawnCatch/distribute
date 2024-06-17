b125480455d689008cd94d1663d9fbb394a174d8
zhou03 <1093826740@qq.com>
2024-06-17 18:41:45 +0800

    feat(UI): 改变前端框架为Flutter
    +迁移框架

--- distribute-ui/src/utils/secure.ts
+++
@@ -1,9 +0,0 @@
-function getToken(): string {
-    return window.localStorage.getItem("token") || "";
-}
-
-function setToken(value: string): void {
-    window.localStorage.setItem("token", value);
-}
-
-export { getToken, setToken };
